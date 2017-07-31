1b5746047a21100a22348393a30bd98f4c842eb6
Yang Zhao <yanzhao@microsoft.com>
2017-07-31 10:40:52 -0700

    Add email me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+//simport {CORE_DIRECTIVES, NgClass, Control, ControlGroup, FormBuilder, Validators} from '@angular/common';
+//import {FORM_DIRECTIVES} from '@angular/forms';
+
+
+class DemoInfo{
+  name:string;
+  password: string; 
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //directives: [CORE_DIRECTIVES, FORM_DIRECTIVES] 
 })
 export class AppComponent {
   title = 'Yang\'s Homepage';
+
+  //CreateGroup: FormBuilder;
+  demoInfo: DemoInfo;
+
+  /*
+  Constructor(fb: FormBuilder){
+    this.demoInfo= new DemoInfo(); 
+    
+    this.CreateGroup = fb.group({
+			'name': new Control(this.demoInfo.name, Validators.required),
+			'password': new Control(this.demoInfo.password, Validators.required)
+		})
+  }
+
+  SendMail(demoInfo:demoInfo) {
+    console.log(demoInfo, 'whole object');
+    this.demoInfo= new DemoInfo();
+  }
+  */
 }
